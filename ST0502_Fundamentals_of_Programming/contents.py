bd2085f9b84b5ca77cffaeb48ba8aec02af9436b
AKS <60446729+AngKS@users.noreply.github.com>
2020-10-12 12:18:23 +0800

    Updated notebook
    Previous version of the notebook was unable to be displayed correctly in the repository

---
+++ ST0502_Fundamentals_of_Programming/contents.py
@@ -0,0 +1,14 @@
+Contents = {
+    "Topic 01" : "Problem Solving Skills",
+    "Topic 02" : "Intro to Computers, Programs & Javascript",
+    "Topic 03": "Data Types & Operators",
+    "Topic 04": "Selections",
+    "Topic 05": "Repetitions",
+    "Topic 06": "Arrays",
+    "Topic 07": "Functions",
+    "Topic 08": "Objects & Classes",
+}
+
+print("###	TABLE OF CONTENTS	###\n")
+for topic in Contents:
+    print(topic + " : " + Contents[topic])
